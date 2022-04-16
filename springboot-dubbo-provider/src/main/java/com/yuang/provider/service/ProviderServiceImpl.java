6b6546e480d2529698bfefb11a537af1fe8caec8
绪风 <54672094+yuang880@users.noreply.github.com>
2022-04-16 21:41:32 +0800

    Update ProviderServiceImpl.java

--- springboot-dubbo-provider/src/main/java/com/yuang/provider/service/ProviderServiceImpl.java
+++ springboot-dubbo-provider/src/main/java/com/yuang/provider/service/ProviderServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 /**
  * @author zhang
  */
-@Component
+@Component   # 可以不加
 @Service(version = "1.0.0")
 public class ProviderServiceImpl implements ProviderService {
     @Override
